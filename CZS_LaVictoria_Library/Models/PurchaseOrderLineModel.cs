c27bd93f7bc672d9726d2894b616b40a5bc6a6f9
Gerardo Mondragón <40957101+JerryMN@users.noreply.github.com>
2021-07-06 18:37:47 -0500

    implement INotifyPropertyChanged

--- CZS_LaVictoria_Library/Models/PurchaseOrderLineModel.cs
+++ CZS_LaVictoria_Library/Models/PurchaseOrderLineModel.cs
@@ -1,16 +1,93 @@
-﻿namespace CZS_LaVictoria_Library.Models
+﻿using System;
+using System.ComponentModel;
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedMember.Global
+
+namespace CZS_LaVictoria_Library.Models
 {
-    public class PurchaseOrderLineModel
+    public class PurchaseOrderLineModel : INotifyPropertyChanged
     {
-        public long NumOrden { get; private set; }
-        public int NumLinea { get; set; }
-        public string Producto { get; set; }
-        public double CantidadOrden { get; set; }
-        public double CantidadEntregada { get; set; }
-        public double CantidadPendiente { get; set; }
-        public decimal PrecioUnitario { get; set; }
-        public bool Iva { get; set; }
-        public decimal Subtotal { get; set; }
+        int _numLinea;
+        string _producto;
+        double _cantidadOrden;
+        double _cantidadRecibida;
+        double _cantidadPendiente;
+        decimal _precioUnitario;
+        bool _iva;
+        decimal _subtotal;
+        DateTime _fechaEntrega;
+        DateTime _fechaUltRecepción;
+        DateTime _fechaCancelación;
+
+        public string Proveedor { get; set; }
+        public string Area { get; set; }
+        public long NumOrden { get; set; }
+
+        public int NumLinea
+        {
+            get => _numLinea;
+            set { _numLinea = value; RaisePropertyChanged("NumLinea"); }
+        }
+
+        public string Producto
+        {
+            get => _producto;
+            set { _producto = value; RaisePropertyChanged("Producto"); }
+        }
+
+        public double CantidadOrden
+        {
+            get => _cantidadOrden;
+            set { _cantidadOrden = value; RaisePropertyChanged("CantidadOrden"); }
+        }
+
+        public double CantidadRecibida
+        {
+            get => _cantidadRecibida;
+            set { _cantidadRecibida = value; RaisePropertyChanged("CantidadRecibida"); }
+        }
+
+        public double CantidadPendiente
+        {
+            get => _cantidadPendiente;
+            set { _cantidadPendiente = value; RaisePropertyChanged("CantidadPendiente"); }
+        }
+
+        public decimal PrecioUnitario
+        {
+            get => _precioUnitario;
+            set { _precioUnitario = value; RaisePropertyChanged("PrecioUnitario"); }
+        }
+
+        public bool Iva
+        {
+            get => _iva;
+            set { _iva = value; RaisePropertyChanged("Iva"); }
+        }
+
+        public decimal Subtotal
+        {
+            get => _subtotal;
+            set { _subtotal = value; RaisePropertyChanged("Subtotal"); }
+        }
+
+        public DateTime FechaEntrega
+        {
+            get => _fechaEntrega;
+            set { _fechaEntrega = value; RaisePropertyChanged("FechaEntrega"); }
+        }
+
+        public DateTime FechaUltRecepción
+        {
+            get => _fechaUltRecepción;
+            set { _fechaUltRecepción = value; RaisePropertyChanged("FechaUltRecepción"); }
+        }
+
+        public DateTime FechaCancelación
+        {
+            get => _fechaCancelación;
+            set { _fechaCancelación = value; RaisePropertyChanged("FechaCancelación"); }
+        }
 
         public PurchaseOrderLineModel()
         {
@@ -26,6 +103,13 @@
             PrecioUnitario = decimal.Parse(precio);
             Iva = false;
             Subtotal = (decimal) CantidadOrden * PrecioUnitario;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void RaisePropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
         }
     }
 }
