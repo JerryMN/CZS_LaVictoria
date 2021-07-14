ca4381ecf5b155e0a3fa39354501cf504022d5a8
Gerardo Mondragón <40957101+JerryMN@users.noreply.github.com>
2021-07-14 14:18:47 -0500

    format

--- CZS_LaVictoria/AlgodónPage/RegistrarCardasForm.Designer.cs
+++ CZS_LaVictoria/AlgodónPage/RegistrarCardasForm.Designer.cs
@@ -30,6 +30,23 @@ namespace CZS_LaVictoria.AlgodónPage
         private void InitializeComponent()
         {
             this.SubtitleLabel = new System.Windows.Forms.Label();
+            this.MsgBox = new FontAwesome.Sharp.IconButton();
+            this.GuardarButton = new System.Windows.Forms.Button();
+            this.CantidadSalidaText = new Syncfusion.WinForms.Input.SfNumericTextBox();
+            this.SalidaLabel = new System.Windows.Forms.Label();
+            this.ArrowButton = new FontAwesome.Sharp.IconButton();
+            this.EntradaLabel = new System.Windows.Forms.Label();
+            this.CantidadEntradaUnidad = new System.Windows.Forms.Label();
+            this.CantidadEntradaText = new Syncfusion.WinForms.Input.SfNumericTextBox();
+            this.TurnoText = new System.Windows.Forms.NumericUpDown();
+            this.MaquinaText = new System.Windows.Forms.NumericUpDown();
+            this.FechaPicker = new Syncfusion.WinForms.Input.SfDateTimeEdit();
+            this.OperadorCombo = new System.Windows.Forms.ComboBox();
+            this.MaquinaLabel = new System.Windows.Forms.Label();
+            this.OperadorLabel = new System.Windows.Forms.Label();
+            this.FechaLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.TurnoText)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MaquinaText)).BeginInit();
             this.SuspendLayout();
             // 
             // SubtitleLabel
@@ -42,17 +59,219 @@ namespace CZS_LaVictoria.AlgodónPage
             this.SubtitleLabel.TabIndex = 32;
             this.SubtitleLabel.Text = "Registro de Cardas";
             // 
+            // MsgBox
+            // 
+            this.MsgBox.FlatAppearance.BorderSize = 0;
+            this.MsgBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MsgBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MsgBox.IconChar = FontAwesome.Sharp.IconChar.ExclamationTriangle;
+            this.MsgBox.IconColor = System.Drawing.Color.DarkRed;
+            this.MsgBox.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.MsgBox.IconSize = 32;
+            this.MsgBox.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.MsgBox.Location = new System.Drawing.Point(580, 250);
+            this.MsgBox.Name = "MsgBox";
+            this.MsgBox.Size = new System.Drawing.Size(420, 165);
+            this.MsgBox.TabIndex = 125;
+            this.MsgBox.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.MsgBox.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.MsgBox.UseVisualStyleBackColor = true;
+            this.MsgBox.Visible = false;
+            // 
+            // GuardarButton
+            // 
+            this.GuardarButton.AutoSize = true;
+            this.GuardarButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GuardarButton.Location = new System.Drawing.Point(922, 200);
+            this.GuardarButton.Name = "GuardarButton";
+            this.GuardarButton.Size = new System.Drawing.Size(78, 35);
+            this.GuardarButton.TabIndex = 124;
+            this.GuardarButton.Text = "Guardar";
+            this.GuardarButton.UseVisualStyleBackColor = true;
+            // 
+            // CantidadSalidaText
+            // 
+            this.CantidadSalidaText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.CantidadSalidaText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CantidadSalidaText.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.CantidadSalidaText.Location = new System.Drawing.Point(750, 147);
+            this.CantidadSalidaText.MinValue = 0D;
+            this.CantidadSalidaText.Name = "CantidadSalidaText";
+            this.CantidadSalidaText.Size = new System.Drawing.Size(250, 29);
+            this.CantidadSalidaText.Style.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.CantidadSalidaText.TabIndex = 123;
+            this.CantidadSalidaText.WatermarkText = "Ingresa la cantidad";
+            // 
+            // SalidaLabel
+            // 
+            this.SalidaLabel.AutoSize = true;
+            this.SalidaLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SalidaLabel.Location = new System.Drawing.Point(600, 150);
+            this.SalidaLabel.Name = "SalidaLabel";
+            this.SalidaLabel.Size = new System.Drawing.Size(125, 21);
+            this.SalidaLabel.TabIndex = 120;
+            this.SalidaLabel.Text = "Tambos Grandes";
+            // 
+            // ArrowButton
+            // 
+            this.ArrowButton.FlatAppearance.BorderSize = 0;
+            this.ArrowButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ArrowButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ArrowButton.IconChar = FontAwesome.Sharp.IconChar.LongArrowAltRight;
+            this.ArrowButton.IconColor = System.Drawing.Color.Black;
+            this.ArrowButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.ArrowButton.Location = new System.Drawing.Point(500, 200);
+            this.ArrowButton.Name = "ArrowButton";
+            this.ArrowButton.Size = new System.Drawing.Size(44, 36);
+            this.ArrowButton.TabIndex = 117;
+            this.ArrowButton.UseVisualStyleBackColor = true;
+            // 
+            // EntradaLabel
+            // 
+            this.EntradaLabel.AutoSize = true;
+            this.EntradaLabel.Location = new System.Drawing.Point(50, 300);
+            this.EntradaLabel.Name = "EntradaLabel";
+            this.EntradaLabel.Size = new System.Drawing.Size(121, 21);
+            this.EntradaLabel.TabIndex = 115;
+            this.EntradaLabel.Text = "Algodón Limpio";
+            // 
+            // CantidadEntradaUnidad
+            // 
+            this.CantidadEntradaUnidad.AutoSize = true;
+            this.CantidadEntradaUnidad.Location = new System.Drawing.Point(455, 300);
+            this.CantidadEntradaUnidad.Name = "CantidadEntradaUnidad";
+            this.CantidadEntradaUnidad.Size = new System.Drawing.Size(27, 21);
+            this.CantidadEntradaUnidad.TabIndex = 114;
+            this.CantidadEntradaUnidad.Text = "kg";
+            // 
+            // CantidadEntradaText
+            // 
+            this.CantidadEntradaText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.CantidadEntradaText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CantidadEntradaText.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.CantidadEntradaText.Location = new System.Drawing.Point(200, 297);
+            this.CantidadEntradaText.MinValue = 0D;
+            this.CantidadEntradaText.Name = "CantidadEntradaText";
+            this.CantidadEntradaText.Size = new System.Drawing.Size(250, 29);
+            this.CantidadEntradaText.Style.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.CantidadEntradaText.TabIndex = 113;
+            this.CantidadEntradaText.WatermarkText = "Ingresa la cantidad";
+            // 
+            // TurnoText
+            // 
+            this.TurnoText.Location = new System.Drawing.Point(330, 247);
+            this.TurnoText.Maximum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.TurnoText.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.TurnoText.Name = "TurnoText";
+            this.TurnoText.Size = new System.Drawing.Size(120, 29);
+            this.TurnoText.TabIndex = 110;
+            this.TurnoText.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // MaquinaText
+            // 
+            this.MaquinaText.Location = new System.Drawing.Point(200, 247);
+            this.MaquinaText.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.MaquinaText.Name = "MaquinaText";
+            this.MaquinaText.Size = new System.Drawing.Size(120, 29);
+            this.MaquinaText.TabIndex = 111;
+            this.MaquinaText.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // FechaPicker
+            // 
+            this.FechaPicker.AllowValueChangeOnMouseWheel = true;
+            this.FechaPicker.Location = new System.Drawing.Point(200, 147);
+            this.FechaPicker.Name = "FechaPicker";
+            this.FechaPicker.Size = new System.Drawing.Size(250, 33);
+            this.FechaPicker.TabIndex = 109;
+            // 
+            // OperadorCombo
+            // 
+            this.OperadorCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.OperadorCombo.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OperadorCombo.FormattingEnabled = true;
+            this.OperadorCombo.Location = new System.Drawing.Point(200, 197);
+            this.OperadorCombo.Name = "OperadorCombo";
+            this.OperadorCombo.Size = new System.Drawing.Size(250, 29);
+            this.OperadorCombo.TabIndex = 108;
+            // 
+            // MaquinaLabel
+            // 
+            this.MaquinaLabel.AutoSize = true;
+            this.MaquinaLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaquinaLabel.Location = new System.Drawing.Point(50, 250);
+            this.MaquinaLabel.Name = "MaquinaLabel";
+            this.MaquinaLabel.Size = new System.Drawing.Size(126, 21);
+            this.MaquinaLabel.TabIndex = 105;
+            this.MaquinaLabel.Text = "Máquina / Turno";
+            // 
+            // OperadorLabel
+            // 
+            this.OperadorLabel.AutoSize = true;
+            this.OperadorLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OperadorLabel.Location = new System.Drawing.Point(50, 200);
+            this.OperadorLabel.Name = "OperadorLabel";
+            this.OperadorLabel.Size = new System.Drawing.Size(77, 21);
+            this.OperadorLabel.TabIndex = 106;
+            this.OperadorLabel.Text = "Operador";
+            // 
+            // FechaLabel
+            // 
+            this.FechaLabel.AutoSize = true;
+            this.FechaLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FechaLabel.Location = new System.Drawing.Point(50, 150);
+            this.FechaLabel.Name = "FechaLabel";
+            this.FechaLabel.Size = new System.Drawing.Size(144, 21);
+            this.FechaLabel.TabIndex = 107;
+            this.FechaLabel.Text = "Fecha de operación";
+            // 
             // RegistrarCardasForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1276, 839);
+            this.Controls.Add(this.MsgBox);
+            this.Controls.Add(this.GuardarButton);
+            this.Controls.Add(this.CantidadSalidaText);
+            this.Controls.Add(this.SalidaLabel);
+            this.Controls.Add(this.ArrowButton);
+            this.Controls.Add(this.EntradaLabel);
+            this.Controls.Add(this.CantidadEntradaUnidad);
+            this.Controls.Add(this.CantidadEntradaText);
+            this.Controls.Add(this.TurnoText);
+            this.Controls.Add(this.MaquinaText);
+            this.Controls.Add(this.FechaPicker);
+            this.Controls.Add(this.OperadorCombo);
+            this.Controls.Add(this.MaquinaLabel);
+            this.Controls.Add(this.OperadorLabel);
+            this.Controls.Add(this.FechaLabel);
             this.Controls.Add(this.SubtitleLabel);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "RegistrarCardasForm";
             this.Text = "RegistrarCardasForm";
+            ((System.ComponentModel.ISupportInitialize)(this.TurnoText)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MaquinaText)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -61,5 +280,20 @@ namespace CZS_LaVictoria.AlgodónPage
         #endregion
 
         private System.Windows.Forms.Label SubtitleLabel;
+        private FontAwesome.Sharp.IconButton MsgBox;
+        private System.Windows.Forms.Button GuardarButton;
+        private Syncfusion.WinForms.Input.SfNumericTextBox CantidadSalidaText;
+        private System.Windows.Forms.Label SalidaLabel;
+        private FontAwesome.Sharp.IconButton ArrowButton;
+        private System.Windows.Forms.Label EntradaLabel;
+        private System.Windows.Forms.Label CantidadEntradaUnidad;
+        private Syncfusion.WinForms.Input.SfNumericTextBox CantidadEntradaText;
+        private System.Windows.Forms.NumericUpDown TurnoText;
+        private System.Windows.Forms.NumericUpDown MaquinaText;
+        private Syncfusion.WinForms.Input.SfDateTimeEdit FechaPicker;
+        private System.Windows.Forms.ComboBox OperadorCombo;
+        private System.Windows.Forms.Label MaquinaLabel;
+        private System.Windows.Forms.Label OperadorLabel;
+        private System.Windows.Forms.Label FechaLabel;
     }
 }
