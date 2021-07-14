6035393d851eb44f7241e09edef794c1e58fb48f
Gerardo Mondragón <40957101+JerryMN@users.noreply.github.com>
2021-07-14 14:00:01 -0500

    add model

---
+++ CZS_LaVictoria_Library/Models/ProducciónAlgodónModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CZS_LaVictoria_Library.Models
+{
+    public class ProducciónAlgodónModel
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Proceso { get; set; }
+        public int Turno { get; set; }
+        public int Máquina { get; set; }
+        public string Operador { get; set; }
+        public string MaterialEntra { get; set; }
+        public double CantidadEntra { get; set; }
+        public string MaterialSale { get; set; }
+        public double CantidadSale { get; set; }
+    }
+}
