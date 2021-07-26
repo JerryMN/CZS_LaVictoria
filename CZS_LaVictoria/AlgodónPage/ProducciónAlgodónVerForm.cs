19cfd58a9cbfdf0cf9e0aec8ddd1d5c5aa34ad48
Gerardo Mondragón <40957101+JerryMN@users.noreply.github.com>
2021-07-26 18:34:44 -0500

    filter by date

--- CZS_LaVictoria/AlgodónPage/ProducciónAlgodónVerForm.cs
+++ CZS_LaVictoria/AlgodónPage/ProducciónAlgodónVerForm.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
+using System.Globalization;
 using System.Windows.Forms;
 using CZS_LaVictoria_Library;
 using CZS_LaVictoria_Library.Models;
@@ -20,7 +23,14 @@ namespace CZS_LaVictoria.AlgodónPage
             DataGrid.Style.CellStyle.Font = new GridFontInfo(new Font("Segoe UI", 12));
             DataGrid.Style.HeaderStyle.Font = new GridFontInfo(new Font("Segoe UI", 12));
             DataGrid.AutoSizeColumnsMode = AutoSizeColumnsMode.AllCells;
+            DesdePicker.Culture = new CultureInfo("es-MX");
+            HastaPicker.Culture = new CultureInfo("es-MX");
+            DesdePicker.Value = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
+            HastaPicker.Value = new DateTime(DateTime.Today.Year, DateTime.Today.Month,
+                DateTime.DaysInMonth(DateTime.Today.Year, DateTime.Today.Month));
         }
+
+        #region Events
 
         void DataGrid_AutoGeneratingColumn(object sender, AutoGeneratingColumnArgs e)
         {
@@ -46,9 +56,41 @@ namespace CZS_LaVictoria.AlgodónPage
             }
         }
 
+        void TodosCheck_CheckedChanged(object sender, EventArgs e)
+        {
+            if (TodosCheck.Checked)
+            {
+                DataGrid.DataSource = LoadTable();
+                DesdePicker.Enabled = false;
+                HastaPicker.Enabled = false;
+                BuscarButton.Enabled = false;
+            }
+            else
+            {
+                DesdePicker.Enabled = true;
+                HastaPicker.Enabled = true;
+                BuscarButton.Enabled = true;
+            }
+        }
+
+        void BuscarButton_Click(object sender, EventArgs e)
+        {
+            Debug.Assert(DesdePicker.Value != null, "DesdePicker.Value != null");
+            Debug.Assert(HastaPicker.Value != null, "HastaPicker.Value != null");
+            DataGrid.DataSource =
+                GlobalConfig.Connection.WoolProduction_GetByDate((DateTime)DesdePicker.Value,
+                    (DateTime)HastaPicker.Value);
+        }
+
+        #endregion
+
+        #region Methods
+
         static List<ProducciónAlgodónModel> LoadTable()
         {
             return GlobalConfig.Connection.WoolProduction_GetAll();
         }
+
+        #endregion
     }
 }
