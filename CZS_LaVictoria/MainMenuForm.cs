a984fcdb58fbdb952f49396c2d852fe33367600b
Gerardo Mondragón <40957101+JerryMN@users.noreply.github.com>
2021-07-28 16:49:32 -0500

    add version number on splash screen

--- CZS_LaVictoria/MainMenuForm.cs
+++ CZS_LaVictoria/MainMenuForm.cs
@@ -31,7 +31,7 @@ namespace CZS_LaVictoria
             var op = new FluentSplashScreenOptions
             {
                 Title = "Escobas La Victoria",
-                Subtitle = "Por CZ Systems",
+                Subtitle = "Por CZ Systems. Versión 1.0",
                 RightFooter = "Iniciando...",
                 LeftFooter = "Copyright © 2021 CZ Systems \nTodos los derechos reservados.",
                 LoadingIndicatorType = FluentLoadingIndicatorType.Dots,
